7f2174c35622e7cf130f08630bb1000efa78978f
Sebastián Ramírez <tiangolo@gmail.com>
2020-04-06 15:35:40 +0200

    :white_check_mark: Add tests for custom prestart script

--- tests/test_02_app/test_custom_app.py
+++ tests/test_02_app/test_custom_app.py
@@ -17,7 +17,7 @@ from ..utils import (
 client = docker.from_env()
 
 
-def verify_container(container, response_text):
+def verify_container(container, response_text, prestart_str):
     config_data = get_config(container)
     assert config_data["workers_per_core"] == 1
     assert config_data["host"] == "0.0.0.0"
@@ -26,23 +26,40 @@ def verify_container(container, response_text):
     assert config_data["workers"] >= 2
     assert config_data["bind"] == "0.0.0.0:80"
     logs = get_logs(container)
-    assert "Checking for script in /app/prestart.sh" in logs
-    assert "Running script /app/prestart.sh" in logs
-    assert (
-        "Running inside /app/prestart.sh, you could add migrations to this file" in logs
-    )
+    assert prestart_str in logs
     response = requests.get("http://127.0.0.1:8000")
     assert response.text == response_text
 
 
 @pytest.mark.parametrize(
-    "environment",
+    "environment,prestart_str",
     [
-        {"MODULE_NAME": "custom_app.custom_main", "VARIABLE_NAME": "custom_var"},
-        {"APP_MODULE": "custom_app.custom_main:custom_var"},
+        (
+            {"MODULE_NAME": "custom_app.custom_main", "VARIABLE_NAME": "custom_var"},
+            "Running inside /app/prestart.sh, you could add migrations to this file",
+        ),
+        (
+            {"APP_MODULE": "custom_app.custom_main:custom_var"},
+            "Running inside /app/prestart.sh, you could add migrations to this file",
+        ),
+        (
+            {
+                "MODULE_NAME": "custom_app.custom_main",
+                "VARIABLE_NAME": "custom_var",
+                "PRE_START_PATH": "/app/custom_app/custom-prestart.sh",
+            },
+            "Custom prestart script",
+        ),
+        (
+            {
+                "APP_MODULE": "custom_app.custom_main:custom_var",
+                "PRE_START_PATH": "/app/custom_app/custom-prestart.sh",
+            },
+            "Custom prestart script",
+        ),
     ],
 )
-def test_custom_app(environment):
+def test_custom_app(environment, prestart_str):
     name = os.getenv("NAME")
     dockerfile = f"{name}.dockerfile"
     response_text = os.getenv("TEST_STR2")
@@ -59,11 +76,11 @@ def test_custom_app(environment):
         detach=True,
     )
     time.sleep(sleep_time)
-    verify_container(container, response_text)
+    verify_container(container, response_text, prestart_str)
     container.stop()
     # Test that everything works after restarting too
     container.start()
     time.sleep(sleep_time)
-    verify_container(container, response_text)
+    verify_container(container, response_text, prestart_str)
     container.stop()
     container.remove()
