24dd544aded3385b54abdebb4ef35b19806feb5f
Sebastián Ramírez <tiangolo@gmail.com>
2020-04-27 10:48:02 +0200

    :construction_worker: Add GitHub actions for CI (#37)
    * :construction_worker: Try to add GitHub action for CI
    
    * :wrench: Add env vars to GitHub action
    
    * :recycle: Deduplicate and simplify build
    
    * :whale: Add Docker deploy to GitHub action
    
    * :lock: Add Docker auth for deploy
    
    * :construction_worker: Disable Travis, keep backup

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -5,11 +5,11 @@ set -e
 use_tag="tiangolo/uvicorn-gunicorn:$NAME"
 use_dated_tag="${use_tag}-$(date -I)"
 
-DOCKERFILE=${DOCKERFILE-$NAME}
-
-docker build -t "$use_tag" --file "./docker-images/${DOCKERFILE}.dockerfile" "./docker-images/"
+bash scripts/build.sh
 
 docker tag "$use_tag" "$use_dated_tag"
+
+bash scripts/docker-login.sh
 
 docker push "$use_tag"
 docker push "$use_dated_tag"
