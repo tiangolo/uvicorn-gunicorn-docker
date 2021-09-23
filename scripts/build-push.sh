687cfdd9fd258b27b037e2c079518b217762f294
Robert Hafner <tedivm@tedivm.com>
2021-09-23 10:37:16 -0500

    Build containers for multiple architectures

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -2,14 +2,23 @@
 
 set -e
 
+DOCKERFILE="$NAME"
+
+if [ "$NAME" == "latest" ] ; then
+    DOCKERFILE="python3.9"
+fi
+
 use_tag="tiangolo/uvicorn-gunicorn:$NAME"
 use_dated_tag="${use_tag}-$(date -I)"
 
-bash scripts/build.sh
-
-docker tag "$use_tag" "$use_dated_tag"
-
 bash scripts/docker-login.sh
 
-docker push "$use_tag"
-docker push "$use_dated_tag"
+docker buildx create --use
+
+docker buildx build \
+  --platform linux/amd64,linux/arm64,linux/arm/v7 \
+  --file "./docker-images/${DOCKERFILE}.dockerfile" \
+  -t "$use_tag"  \
+  -t "$use_dated_tag"  \
+  --push \
+  "./docker-images/"
