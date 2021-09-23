3792e289df1f450ee517a9499f82851dbc8fcf6d
Robert Hafner <tedivm@tedivm.com>
2021-09-23 12:25:49 -0500

    Only publish amd64 architecture with slim builds

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -8,6 +8,13 @@ if [ "$NAME" == "latest" ] ; then
     DOCKERFILE="python3.9"
 fi
 
+# Slim images are missing the build tools for uvloop
+if [[ "$NAME" =~ "slim" ]] ; then
+    PLATFORMS="linux/amd64"
+else
+    PLATFORMS="linux/amd64,linux/arm64,linux/arm/v7"
+fi
+
 use_tag="tiangolo/uvicorn-gunicorn:$NAME"
 use_dated_tag="${use_tag}-$(date -I)"
 
@@ -16,7 +23,7 @@ bash scripts/docker-login.sh
 docker buildx create --use
 
 docker buildx build \
-  --platform linux/amd64,linux/arm64,linux/arm/v7 \
+  --platform $PLATFORMS \
   --file "./docker-images/${DOCKERFILE}.dockerfile" \
   -t "$use_tag"  \
   -t "$use_dated_tag"  \
