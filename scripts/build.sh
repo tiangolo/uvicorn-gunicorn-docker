062ad784e3c31ae2605aae27863170b4ad0b04a3
monk-ee <magic.monkee.magic@gmail.com>
2022-06-06 15:23:10 +1000

    feat: bump python and debian - add build steps

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-use_tag="tiangolo/uvicorn-gunicorn:$NAME"
+use_tag="monkeemagic/uvicorn-gunicorn:$NAME"
 
 DOCKERFILE="$NAME"
 
 if [ "$NAME" == "latest" ] ; then
-    DOCKERFILE="python3.9"
+    DOCKERFILE="python3.10"
 fi
 
 docker build -t "$use_tag" --file "./docker-images/${DOCKERFILE}.dockerfile" "./docker-images/"
